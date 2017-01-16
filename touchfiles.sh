eebeba8731b4d69e6dabfc9063af15f32c468ace
ymc023@163.com <ymc023@163.com>
2017-01-16 09:33:05 +0800

    add touchfiles.sh

---
+++ touchfiles.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#Author:
+#Mail: 
+#Platform:
+#Date:Mon 16 Jan 2017 05:30:29 AM CST
+
+function touchfiles(){
+for i in `seq 10`:
+do
+    touch `echo -n $RANDOM|md5sum|cut -c 1-10|tr '0-9' 'a-j'`_ymc023.txt
+done
+
+}
+touchfiles
