3ffd3e1a46cdd7fcbb7f69fbae11add3031c5297
ymc023@163.com <ymc023@163.com>
2017-01-16 09:32:47 +0800

    add userdel.sh

---
+++ userdel.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#Author:
+#Mail: 
+#Platform:
+#Date:Mon 16 Jan 2017 05:43:51 AM CST
+
+function user_del(){
+
+for i in $(seq -w 15)
+do
+    userdel -r  ymc0$i
+    if [ $? == 0 ];then
+        echo "ymc0$i delete ok."
+    fi
+done
+}
+
+user_del
